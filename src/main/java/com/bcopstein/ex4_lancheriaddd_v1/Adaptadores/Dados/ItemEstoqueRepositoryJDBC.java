de32abcf94e3f08aeb38e167e3549d4f6dcf290d
jucewicz <p.jucewicz@edu.pucrs.br>
2025-10-02 11:09:04 -0300

    itemEstoqueRepositoryJDBC feito

---
+++ src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Adaptadores/Dados/ItemEstoqueRepositoryJDBC.java
@@ -0,0 +1,28 @@
+package com.bcopstein.ex4_lancheriaddd_v1.Adaptadores.Dados;
+
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Dados.ItemEstoqueRepository;
+import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Entidades.Ingrediente;
+import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Entidades.ItemEstoque;
+
+public class ItemEstoqueRepositoryJDBC implements ItemEstoqueRepository {
+    private JdbcTemplate jdbcTemplate;
+
+    public ItemEstoqueRepositoryJDBC(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<ItemEstoque> getAll() {
+        List<ItemEstoque> resp = jdbcTemplate.query(
+            "SELECT id, quantidade, ingrediente_id FROM itensEstoque",
+            (rs, rowNum) -> {
+                Ingrediente ing = new Ingrediente(rs.getLong("ingrediente_id")); // só o id
+                return new ItemEstoque(ing, rs.getInt("quantidade"));
+            }
+        );
+        return resp;
+    }
+}
