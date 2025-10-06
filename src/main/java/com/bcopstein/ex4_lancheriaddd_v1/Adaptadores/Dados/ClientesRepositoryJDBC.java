bdaba63efb9725f8a66d39909c79a3cd8508e072
jucewicz <p.jucewicz@edu.pucrs.br>
2025-10-06 21:50:00 +0000

    ClientesRepositoryJdbc

---
+++ src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Adaptadores/Dados/ClientesRepositoryJDBC.java
@@ -0,0 +1,34 @@
+package com.bcopstein.ex4_lancheriaddd_v1.Adaptadores.Dados;
+
+import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Dados.ClientesRepository;
+import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Entidades.Cliente;
+
+public class ClientesRepositoryJDBC implements ClientesRepository{
+    private JdbcTemplate jdbcTemplate;
+
+
+    public ClientesRepositoryJDBC(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Cliente getByCpf(String cpf){
+        String sql = "SELECT c.cpf, c.nome, c.celular, c.endereco, c.email "+
+                    "FROM CLIENTES c " +
+                    "WHERE c.cpf = ?";
+
+        List<Cliente> clientes = this.jdbcTemplate.query(
+            sql,
+            ps -> ps.setString(1, cpf),
+            (rs, rowNum) -> {
+                String cpfCli = rs.getString("cpf");
+                String nomeCli = rs.getString("nome");
+                String celularCli = rs.getString("celular");
+                String enderecoCli = rs.getString("endereco");
+                String emailCli = rs.getString("email");
+                return new Cliente(cpfCli, nomeCli, celularCli, enderecoCli, emailCli);
+            }
+            );
+        return cliente.isEmpty() ? null : clientes.get(0);
+        
+    }
+}
