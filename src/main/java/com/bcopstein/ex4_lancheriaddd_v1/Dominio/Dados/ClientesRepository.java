1af9e338037016828eb9d9b5e76c9ac9bbbaf721
Nick Fer <nickfer01.ncf@gmail.com>
2025-10-06 18:19:44 -0300

    interface repository clientes

---
+++ src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Dominio/Dados/ClientesRepository.java
@@ -0,0 +1,7 @@
+package com.bcopstein.ex4_lancheriaddd_v1.Dominio.Dados;
+
+import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Entidades.Cliente;
+
+public interface ClientesRepository {
+    Cliente getById(long id);
+}
