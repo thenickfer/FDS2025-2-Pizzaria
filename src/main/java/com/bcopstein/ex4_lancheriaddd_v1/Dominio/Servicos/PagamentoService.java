d40ee4b12003881431350e7345c3e3c2f3d74463
Luiz Augusto Guerra <luizgutoguerra@gmail.com>
2025-10-08 21:47:29 +0000

    PagamentoService

--- src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Dominio/Servicos/PagamentoService.java
+++ src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Dominio/Servicos/PagamentoService.java
@@ -1,5 +1,30 @@
 package com.bcopstein.ex4_lancheriaddd_v1.Dominio.Servicos;
+import org.springframework.stereotype.Service;
 
+import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Dados.PedidoRepository;
+import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Entidades.Pedido;
+
+
+@Service
 public class PagamentoService {
+    private final PedidoRepository pedidoRepository;
 
+    public PagamentoService(PedidoRepository pedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+    }
+
+    public Pedido processarPagamento(Pedido pedido) {
+        if (pedido == null) {
+            throw new IllegalArgumentException("Pedido não pode ser nulo.");
+        }
+
+        if (pedido.getStatus() != Pedido.Status.APROVADO) {
+            throw new IllegalStateException("Somente pedidos aceitos podem ser pagos.");
+        }
+
+        pedido.setStatus(Pedido.Status.PAGO);
+
+        return pedido;
+    }
 }
+
