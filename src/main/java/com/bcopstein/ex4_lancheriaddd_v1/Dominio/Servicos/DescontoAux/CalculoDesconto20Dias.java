87851b35e921e572ca10e93cc9d786babee103c0
Nick Fer <nickfer01.ncf@gmail.com>
2025-11-10 14:09:04 -0300

    testes desconto + fixes

--- src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Dominio/Servicos/DescontoAux/CalculoDesconto20Dias.java
+++ src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Dominio/Servicos/DescontoAux/CalculoDesconto20Dias.java
@@ -2,13 +2,22 @@ package com.bcopstein.ex4_lancheriaddd_v1.Dominio.Servicos.DescontoAux;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Dados.PedidoRepository;
 import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Entidades.Pedido;
 
+@Component
 public class CalculoDesconto20Dias implements CalculoDesconto {
     private static final int QNTD_PARA_DESCONTO = 3;
     private static final double TAXA = 0.07;
     private PedidoRepository pedidoRepository;
+
+    @Autowired
+    public CalculoDesconto20Dias(PedidoRepository pedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+    }
 
     public double calcularDesconto(Pedido p) {
         if (p == null) {
--- src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Dominio/Servicos/DescontoAux/CalculoDesconto30Dias.java
+++ src/main/java/com/bcopstein/ex4_lancheriaddd_v1/Dominio/Servicos/DescontoAux/CalculoDesconto30Dias.java
@@ -1,14 +1,20 @@
 package com.bcopstein.ex4_lancheriaddd_v1.Dominio.Servicos.DescontoAux;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
 import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Dados.PedidoRepository;
 import com.bcopstein.ex4_lancheriaddd_v1.Dominio.Entidades.Pedido;
 
+@Component
 public class CalculoDesconto30Dias implements CalculoDesconto {
 
     private static final double TAXA = 0.15;
     private static final int QNTD_PARA_DESCONTO = 500;
     private PedidoRepository pedidoRepository;
 
+    @Autowired
     public CalculoDesconto30Dias(PedidoRepository pedidoRepository) {
         this.pedidoRepository = pedidoRepository;
     }
